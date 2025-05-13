df8e706366e481d2586590da0578dee5041691a3
brian.pace <brian.pace@outlook.com>
2025-05-13 12:56:13 -0400

    Updates to test cases

--- test.sh
+++ test.sh
@@ -5,23 +5,25 @@
 #  done
 
 export TEST_PLATFORM=$1
+export OUTPUT_BASE=/app/temp/pgcompare
 echo Test ${TEST_PLATFORM} to Postgres
 cd target
-export PGCOMPARE_LOG_DESTINATION=/app/temp/pgcompare/${TEST_PLATFORM}-full-a-discover.log
+rm ${OUTPUT_BASE}/${TEST_PLATFORM}*
+export PGCOMPARE_LOG_DESTINATION=${OUTPUT_BASE}/${TEST_PLATFORM}-full-a-discover.log
 export PGCOMPARE_CONFIG=/Users/bpace/app/gitecto/db-projects/pgCompare-Test/pgcompare.${TEST_PLATFORM}.properties
-java -jar pgcompare.jar --discover
-export PGCOMPARE_LOG_DESTINATION=/app/temp/pgcompare/${TEST_PLATFORM}-full-b-compare.log
-java -jar pgcompare.jar
-export PGCOMPARE_LOG_DESTINATION=/app/temp/pgcompare/${TEST_PLATFORM}-full-c-check.log
-java -jar pgcompare.jar --check
+java -jar pgcompare.jar --discover --report ${OUTPUT_BASE}/${TEST_PLATFORM}-full-a-discover.html
+export PGCOMPARE_LOG_DESTINATION=${OUTPUT_BASE}/${TEST_PLATFORM}-full-b-compare.log
+java -jar pgcompare.jar --report ${OUTPUT_BASE}/${TEST_PLATFORM}-full-b-compare.html
+export PGCOMPARE_LOG_DESTINATION=${OUTPUT_BASE}/${TEST_PLATFORM}-full-c-check.log
+java -jar pgcompare.jar --check --report ${OUTPUT_BASE}/${TEST_PLATFORM}-full-c-check.html
 
 
-export PGCOMPARE_LOG_DESTINATION=/app/temp/pgcompare/${TEST_PLATFORM}-plat-a-discover.log
-export PGCOMPARE_CONFIG=/Users/bpace/app/gitecto/db-projects/pgCompare-Test/pgcompare.${TEST_PLATFORM}.properties
-java -jar pgcompare.jar --discover --table plat
-export PGCOMPARE_LOG_DESTINATION=/app/temp/pgcompare/${TEST_PLATFORM}-plat-b-compare.log
-java -jar pgcompare.jar --table plat
-export PGCOMPARE_LOG_DESTINATION=/app/temp/pgcompare/${TEST_PLATFORM}-plat-c-check.log
-java -jar pgcompare.jar --check --table plat
+#export PGCOMPARE_LOG_DESTINATION=${OUTPUT_BASE}/${TEST_PLATFORM}-plat-a-discover.log
+#export PGCOMPARE_CONFIG=/Users/bpace/app/gitecto/db-projects/pgCompare-Test/pgcompare.${TEST_PLATFORM}.properties
+#java -jar pgcompare.jar --discover --table plat
+#export PGCOMPARE_LOG_DESTINATION=${OUTPUT_BASE}/${TEST_PLATFORM}-plat-b-compare.log
+#java -jar pgcompare.jar --table plat
+#export PGCOMPARE_LOG_DESTINATION=${OUTPUT_BASE}/${TEST_PLATFORM}-plat-c-check.log
+#java -jar pgcompare.jar --check --table plat
 
 cd -
