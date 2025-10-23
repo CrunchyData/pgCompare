45de866fb77dd8b79a608e2eeb44689c7d2a3761
brian.pace <brian.pace@outlook.com>
2025-10-23 16:41:49 -0400

    models refactor

--- src/main/java/com/crunchydata/models/DCTableColumn.java
+++ src/main/java/com/crunchydata/models/DCTableColumn.java
@@ -1,7 +1,5 @@
-package com.crunchydata.models;
-
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +13,7 @@ package com.crunchydata.models;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.crunchydata.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
